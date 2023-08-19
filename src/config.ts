20f58828c0875e6b9ac8eef72335d9e5731d65a8
Kishan Poddar <nandanpoddar546@gmail.com>
2023-08-19 14:15:50 +0530

    setup cloudinary and form validations with joi

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,8 @@
 import dotenv from "dotenv";
 import bunyan from 'bunyan';
-
+import cloudinary from 'cloudinary'
 
 dotenv.config({});
-
-
 
 class Config {
   public DATABASE_URL: string | undefined;
@@ -14,6 +12,9 @@ class Config {
   public SECRET_KEY_TWO: string | undefined;
   public CLIENT_URL: string | undefined;
   public REDIS_HOST: string | undefined;
+  public CLOUD_API_SECRET: string | undefined;
+  public CLOUD_API_KEY: string | undefined;
+  public CLOUD_NAME: string | undefined;
 
   private readonly DEFAULT_DATABASE_URL =
     "mongodb+srv://nandanpoddar546:bZkaW2dAYnFsaSgG@cluster0.eoruken.mongodb.net/chatty-app?retryWrites=true&w=majority";
@@ -26,6 +27,9 @@ class Config {
     this.SECRET_KEY_TWO = process.env.SECRET_KEY_TWO || "";
     this.CLIENT_URL = process.env.CLIENT_URL || "";
     this.REDIS_HOST = process.env.REDIS_HOST || "";
+    this.CLOUD_NAME = process.env.CLOUD_NAME || "";
+    this.CLOUD_API_KEY = process.env.CLOUD_API_KEY || "";
+    this.CLOUD_API_SECRET = process.env.CLOUD_API_SECRET || "";
   }
 
   public createLogger(name: string): bunyan {
@@ -38,6 +42,14 @@ class Config {
         throw new Error(`Configuration ${key} is undefined.`);
       }
     }
+  }
+
+  public cloudinaryConfig(): void {
+    cloudinary.v2.config({
+      cloud_name: this.CLOUD_NAME,
+      api_key: this.CLOUD_API_KEY,
+      api_secret: this.CLOUD_API_SECRET
+    })
   }
 }
 
